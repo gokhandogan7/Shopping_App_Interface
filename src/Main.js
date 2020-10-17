21c7045e50c903473a1f5b7de7acee49ad974bde
gokhandogan7 <gdogan3746@gmail.com>
2020-10-17 23:25:13 +0200

    Shopping App Page has created

---
+++ src/Main.js
@@ -0,0 +1,58 @@
+/* eslint-disable prettier/prettier */
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
+import productData from './product_data.json';
+import {ProductCard} from './components';
+
+const App = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [displayList, setDisplayList] = useState([]);
+  const renderListItem = ({item}) => <ProductCard product={item} />;
+  useEffect(() => {
+    setDisplayList(productData);
+  }, []);
+
+  useEffect(() => {
+    const filteredValue = productData.filter((item) => {
+      const text = searchValue.toUpperCase();
+      const productTitle = item.title.toUpperCase();
+
+      return productTitle.indexOf(text) > -1;
+    });
+    setDisplayList(filteredValue);
+  }, [searchValue]);
+
+  return (
+    <SafeAreaView>
+      <Text style={styles.banner}>Clarushop</Text>
+      <View sytle={styles.searchBar}>
+        <TextInput placeholder="ürün ara..." onChangeText = {(value) => setSearchValue(value)} />
+      </View>
+      <View>
+        <FlatList
+          keyExtractor={(_, index) => index.toString()}
+          data={displayList}
+          renderItem={renderListItem}
+          numColumns={2}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
+
+const styles = StyleSheet.create({
+  banner: {
+    fontSize: 30,
+    color: 'purple',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable semi */
+export * from './ProductCard';
